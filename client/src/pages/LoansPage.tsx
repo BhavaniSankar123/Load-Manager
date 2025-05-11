9d214a417613cd4c6b5b63f4475bba28a2163704
dilnalla <dilnalla29@gmail.com>
2025-05-12 01:16:06 +0530

    commit

--- client/src/pages/LoansPage.tsx
+++ client/src/pages/LoansPage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { api } from '../services/api';
 import LoansTable from '../components/LoansDashboard/LoansTable';
+import { api } from '../services/api';
 import ".././index.css"
 
 const LoansPage: React.FC = () => {
